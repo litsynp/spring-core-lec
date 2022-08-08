d5c9ea00d10b37b967b59afd22ebcdb5c2a44443
litsynp <nocte55is@gmail.com>
2022-08-08 19:11:13 +0900

    refactor: refactor AppConfig
    - Remove duplicate codes
    - Make it easier to grasp roles and implementations

--- src/main/java/com/litsynp/core/AppConfig.java
+++ src/main/java/com/litsynp/core/AppConfig.java
@@ -1,19 +1,32 @@
 package com.litsynp.core;
 
-import com.litsynp.core.discount.FixDiscountPolicy;
+import com.litsynp.core.discount.DiscountPolicy;
+import com.litsynp.core.discount.RateDiscountPolicy;
+import com.litsynp.core.member.MemberRepository;
 import com.litsynp.core.member.MemberService;
 import com.litsynp.core.member.MemberServiceImpl;
 import com.litsynp.core.member.MemoryMemberRepository;
 import com.litsynp.core.order.OrderService;
 import com.litsynp.core.order.OrderServiceImpl;
 
+// 사용 영역과 구성 영역의 분리
+// Discount Policy가 달라져도 사용 영역의 코드는 전혀 변경할 필요가 없다.
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
