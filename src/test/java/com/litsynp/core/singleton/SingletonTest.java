8672f38ffe2388b3d4dab931e333612502cbd984
litsynp <nocte55is@gmail.com>
2022-08-09 00:55:08 +0900

    test: test stateful singleton service

--- src/test/java/com/litsynp/core/singleton/SingletonTest.java
+++ src/test/java/com/litsynp/core/singleton/SingletonTest.java
@@ -4,6 +4,8 @@ import com.litsynp.core.AppConfig;
 import com.litsynp.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,5 +38,22 @@ class SingletonTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 1. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService1 = ac.getBean(MemberService.class);
+
+        // 2. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService2 = ac.getBean(MemberService.class);
+
+        // 참조 값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
