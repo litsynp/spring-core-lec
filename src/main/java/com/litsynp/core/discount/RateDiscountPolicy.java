57b90dc6c6e34be220d3f354024145f9ba6810b0
litsynp <nocte55is@gmail.com>
2022-08-08 16:02:52 +0900

    feat: add rated discount policy

---
+++ src/main/java/com/litsynp/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package com.litsynp.core.discount;
+
+import com.litsynp.core.member.Grade;
+import com.litsynp.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
