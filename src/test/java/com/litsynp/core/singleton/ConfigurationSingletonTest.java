d5c75a858fc937726622088096b4fe2ba1da563d
litsynp <nocte55is@gmail.com>
2022-08-09 10:04:36 +0900

    test: test singleton from config

---
+++ src/test/java/com/litsynp/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,32 @@
+package com.litsynp.core.singleton;
+
+import com.litsynp.core.AppConfig;
+import com.litsynp.core.member.MemberRepository;
+import com.litsynp.core.member.MemberServiceImpl;
+import com.litsynp.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        // 세 개 다 같다!
+        System.out.println("memberService -> memberRepository1 = " + memberRepository1);
+        System.out.println("orderService -> memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
