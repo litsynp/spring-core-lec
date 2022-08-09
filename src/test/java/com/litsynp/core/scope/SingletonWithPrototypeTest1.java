a8d9f92dcfd0c0830a9abd1d4a04e945b9b15a6d
litsynp <nocte55is@gmail.com>
2022-08-09 18:19:04 +0900

    test: learn precautions when singleton and prototype beans used together

---
+++ src/test/java/com/litsynp/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,86 @@
+package com.litsynp.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * singleton bean과 prototype bean을 같이 사용할 때 발생하는 주의사항
+ * <p>
+ * 주입 시점에만 prototype bean이 생성된다.
+ */
+class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2);  // inner prototype bean is not created; only referenced when registering client bean
+    }
+
+    @Scope("singleton")  // default
+    static class ClientBean {
+
+        private final PrototypeBean prototypeBean;  // uses prototype bean inside
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean) {
+            // 이 때 prototype bean을 만들어서 주입된다. 따라서 singleton bean인 ClientBean은 같은 prototype bean을 참조한다.
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
