6b73a603337a11501db69211e5e5720b0de75ce0
litsynp <nocte55is@gmail.com>
2022-08-09 10:22:11 +0900

    test: test how cglib works

--- src/main/java/com/litsynp/core/AppConfig.java
+++ src/main/java/com/litsynp/core/AppConfig.java
@@ -13,7 +13,10 @@ import org.springframework.context.annotation.Configuration;
 
 // 사용 영역과 구성 영역의 분리
 // Discount Policy가 달라져도 사용 영역의 코드는 전혀 변경할 필요가 없다.
-@Configuration
+@Configuration  // @Configuration을 붙이지 않으면?
+// 1. CGLIB이 동작하지 않아 일반 AppConfig가 반환된다.
+// 2. 싱글톤이 깨진다. (@Bean이 붙어도 빈으로는 등록되지만 여러 번 객체가 생성되어 싱글톤이 보장되지 않는다.)
+// => 고민하지 말고 @Configuration을 붙여주자.
 public class AppConfig {
 
     // @Bean memberService -> new MemoryMemberRepository()
@@ -21,8 +24,9 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
-
         System.out.println("call AppConfig.memberService");
+        // @Configuration을 붙이지 않으면 CGLIB이 동작하지 않아 memberRepository()가 그대로 호출되므로 싱글톤이 아니다.
+        // @Autowired를 붙여주는 방법도 있다.
         return new MemberServiceImpl(memberRepository());
     }
 
--- src/test/java/com/litsynp/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/com/litsynp/core/singleton/ConfigurationSingletonTest.java
@@ -29,4 +29,13 @@ public class ConfigurationSingletonTest {
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);  // 스프링이 빈을 등록하는 과정에서 바이트코드를 조작 -- 실제로는 AppConfig의 자식 타입
+
+        System.out.println("bean = " + bean.getClass());  // CGLIB 바이트 조작 라이브러리로 해당 클래스를 상속 받은 다른 클래스를 스프링 빈으로 등록한다.
+        // 따라서 AppConfig에서 호출한 클래스가 바로 싱글톤이 보장되도록 한다.  (이미 빈이 등록됐다면 컨테이너에서 찾아서, 등록되지 않았다면 기존 로직을 호출해 새로 객체를 생성해 컨테이너에 등록해 반환)
+    }
 }
