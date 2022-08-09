d95cc530ed9077185cadd68bf56d04d8bee3fdc8
litsynp <nocte55is@gmail.com>
2022-08-09 21:24:32 +0900

    refactor: add proxy with scope

--- src/main/java/com/litsynp/core/common/MyLogger.java
+++ src/main/java/com/litsynp/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package com.litsynp.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,14 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+// 적용 대상이 클래스면 TARGET_CLASS, 인터페이스면 INTERFACES를 선택
+// 이렇게 하면 MyLogger의 가짜 프록시 클래스를 만들어두고 HTTP request와 상관 없이 가짜 프록시 클래스를 다른 빈에 미리 주입해 둘 수 있다.
+// Scope가 request여서 실행 시에 문제가 발생해 추가한 Provider인 ObjectProvider<MyLogger>가 아니라 MyLogger로 바로 주입할 수 있다!
+// - 스프링 컨테이너에 myLogger라는 이름으로 CGLIB으로 생성한 MyLogger 프록시 객체를 등록한다.
+// - *** Provider, 프록시의 핵심 아이디어는 진짜 객체 조회를 꼭 필요한 시점까지 지연처리 한다는 점이다.
+// - 꼭 웹 스코프가 아니어도 프록시는 사용할 수 있다.
+// - 가장 큰 장점은, **클라이언트의 코드를 전혀 바꾸지 않아도 된다는 점이다.**
 public class MyLogger {
 
     private String uuid;
--- src/main/java/com/litsynp/core/web/LogDemoController.java
+++ src/main/java/com/litsynp/core/web/LogDemoController.java
@@ -2,7 +2,6 @@ package com.litsynp.core.web;
 
 import com.litsynp.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,16 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestUrl = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+        System.out.println("myLogger = " + myLogger.getClass());  // 진짜 MyLogger가 아닌, CGLIB이 추가한 MyLogger - 클라이언트는 동일하게 사용할 수 있다 (다형성)
         myLogger.setRequestUrl(requestUrl);
 
-        myLogger.log("controller test");
+        myLogger.log("controller test");  // 프록시 객체는 MyLogger의 진짜 log() 함수를 실행한다.
         Thread.sleep(1000);
         logDemoService.logic("testId");  // 같은 HTTP 요청이면 서비스에서도 같은 UUID가 나올 것이다.
         return "OK";
